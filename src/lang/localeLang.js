d8a9252e51701c1b59224ea2ec4b01c43ba2c75f
Paizurakhman <170107091@stu.sdu.edu.kz>
2021-05-31 12:18:23 +0600

    'fix;

--- src/lang/localeLang.js
+++ src/lang/localeLang.js
@@ -91,7 +91,7 @@ let locale = {
                 text2: 'команда',
             },
             position: 'Опыт ',
-            experience: 'Должност',
+            experience: 'Должность',
         },
         footer: {
             main: 'Меню',
